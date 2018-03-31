60df7e0701c181a646f2d041092179e415390763
Mohammed Saimon <saimon2721@gmail.com>
2018-03-31 16:16:00 +0600

    initial commit

---
+++ customer-wallet-discount.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Plugin Name: Customer Wallet & Discount
+ * Plugin URI: www.saimonsplugins.com
+ * Author: Mohammed saimon
+ * Description: A simple plugin
+ */
+
+if ( ! defined( 'WPINC' ) ) exit;
+
+class Customer_Wallet_Discount {
+    public static $version;
+    public static $instance;
+
+    public function __construct() {
+        self::$version = '1.0';
+        $this->define_constants();
+        $this->init_hooks();
+        $this->includes();
+    }
+
+    public function define_constants() {
+        define( 'CWD_DIR', plugin_dir_path( __FILE__ ) );
+        define( 'CWD_INC', plugin_dir_path( __FILE__ ) . 'includes/' );
+        define( 'CWD_ASSETS', plugin_dir_url( __FILE__ ) . 'assets/' );
+    }
+
+    public function init_hooks() {
+        if ( is_admin() ) {
+
+        } else {
+            add_action( 'wp_enqueue_scripts', array( $this, 'load_customer_wallet_discount_scripts' ) );
+        }
+    }
+
+    public function load_customer_wallet_discount_scripts() {
+        if ( ! is_account_page() ) return;
+
+        wp_enqueue_style( 'customer-wallet-discount', CWD_ASSETS . 'css/style.css' );
+    }
+
+    public function includes() {
+        if ( is_admin() ) {
+
+        } else {
+            require_once CWD_INC . 'public/customer-discount-public.php';
+        }
+
+    }
+
+    public static function init() {
+        if ( is_null( self::$instance ) ) {
+            self::$instance = new Customer_Wallet_Discount();
+        }
+
+        return self::$instance;
+    }
+}
+
+add_action( 'plugins_loaded', array( 'Customer_Wallet_Discount', 'init' ) );
---
+++ includes/public/customer-discount-public.php
@@ -0,0 +1,58 @@
+<?php
+
+if ( ! defined( 'WPINC' ) ) exit;
+
+class Customer_Discount_Public {
+    public static $instance = null;
+
+    public function __construct() {
+        $this->init_hooks();
+    }
+
+    public function init_hooks() {
+        add_action( 'woocommerce_account_dashboard', array( $this, 'customer_discount_details' ) );
+    }
+
+    public function customer_discount_details() {
+        $customer = new WC_Customer( get_current_user_id() );
+        ?>
+        <div class="customer-wallet">
+            <h2 class="ribbon">
+               <strong class="ribbon-content">Your Wallet</strong>
+            </h2>
+            <div class="wallet-details">
+                <div class="avatar">
+                    <img src="<?php echo $customer->get_avatar_url() ?>" alt="Customer Avatar">
+                </div>
+                <div class="balance">
+                    <?php printf( __( 'Current Credits: %s', 'domain' ), $customer->get_total_spent() ); ?>
+                </div>
+                <div class="badge">
+                    <p>badge</p>
+                </div>
+            </div>
+
+        </div>
+        <?php
+
+        // $html = '<div class="customer-discount">';
+        // $html .= '<h2> Your Discount </h2>';
+        // $html .= '<h3> You are a lite user </h3>';
+        // $html .= '<p> Total Orders: ' . wc_get_customer_order_count( get_current_user_id() ) . '</p>';
+        // $html .= '<p> Total Spent: ' . wc_get_customer_total_spent( get_current_user_id() ) . '</p>';
+        // $html .= '<p> You will get $50 discount when your total spent will be greater than $500';
+        // $html .= '</div>';
+        //
+        // echo $html;
+    }
+
+    public static function init() {
+        if ( is_null( self::$instance ) ) {
+            self::$instance = new Self;
+        }
+
+        return self::$instance;
+    }
+}
+
+Customer_Discount_Public::init();
